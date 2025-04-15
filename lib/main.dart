3291a37a74cd8356703e2306755d76b2a55ae5b4
Mosawir Shah <92377115+MosawirShah@users.noreply.github.com>
2025-04-15 15:32:03 +0500

    enumeration is used for Gender selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/result.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +12,21 @@ class BMICalculator extends StatefulWidget {
   State<BMICalculator> createState() => _BMICalculatorState();
 }
 
+enum Gender { male, female }
+
 class _BMICalculatorState extends State<BMICalculator> {
+  Gender? selectedGender;
+
+  ///COLORS
+  Color activeColor = Colors.tealAccent;
+  Color inActiveColor = Colors.teal;
 
   double height = 150;
+  int weight = 74;
+  int age = 18;
   @override
   Widget build(BuildContext context) {
-
-    print("BUILD METHOD CALLED");
+    print("The Value of Var $selectedGender");
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -37,41 +46,65 @@ class _BMICalculatorState extends State<BMICalculator> {
               child: Row(
                 children: [
                   Expanded(
-                      child: CustomContainer(
-                    containerColor: Colors.teal,
-                    myChild: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.male,
-                          size: 70,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Male",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white,fontSize: 18),
-                        ),
-                      ],
-                    ),
-                  )),
-                  Expanded(
+                      child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                        print("The Value of Var $selectedGender");
+                      });
+                    },
                     child: CustomContainer(
-                      containerColor: Colors.teal,
+                      containerColor: selectedGender == Gender.male
+                          ? activeColor
+                          : inActiveColor,
                       myChild: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
-                            Icons.female,
+                            Icons.male,
                             size: 70,
                             color: Colors.white,
                           ),
                           Text(
-                            "Female",
+                            "Male",
                             style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.white,fontSize: 18),
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18),
                           ),
                         ],
+                      ),
+                    ),
+                  )),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                          print("The Value of Var $selectedGender");
+                        });
+                      },
+                      child: CustomContainer(
+                        containerColor: selectedGender == Gender.female
+                            ? activeColor
+                            : inActiveColor,
+                        myChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.female,
+                              size: 70,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Female",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 18),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -87,26 +120,46 @@ class _BMICalculatorState extends State<BMICalculator> {
               myChild: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("HEIGHT",style: TextStyle(fontSize: 18,color: Colors.white60,fontWeight: FontWeight.bold),),
+                  Text(
+                    "HEIGHT",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white60,
+                        fontWeight: FontWeight.bold),
+                  ),
                   Row(
                     textBaseline: TextBaseline.alphabetic,
                     crossAxisAlignment: CrossAxisAlignment.baseline,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                    Text(height.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: Colors.white),),
-                    Text("cm",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.white),)
-                  ],),
-                  Slider(value: height,
-                      max: 180,
-                      min: 120,
-                      divisions: 60,
-                      label: height.toStringAsFixed(1),
-                      onChanged: (value){
-                    setState(() {
-                      height = value;
-                     // print("height :$height");
-                    });
-                      },
+                      Text(
+                        "180",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        "cm",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                  Slider(
+                    value: height,
+                    max: 180,
+                    min: 120,
+                    divisions: 60,
+                    label: height.toStringAsFixed(1),
+                    onChanged: (value) {
+                      setState(() {
+                        height = value;
+                        // print("height :$height");
+                      });
+                    },
                     activeColor: Colors.pink,
                   )
                 ],
@@ -120,16 +173,137 @@ class _BMICalculatorState extends State<BMICalculator> {
                   Expanded(
                       child: CustomContainer(
                     containerColor: Colors.teal,
-                    myChild: Text(""),
+                    myChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "WEIGHT",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  if (weight > 0) {
+                                    weight--;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: Icon(
+                                  Icons.remove,
+                                  color: Colors.teal,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 13,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.teal,
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   )),
                   Expanded(
                       child: CustomContainer(
                     containerColor: Colors.teal,
-                    myChild: Text(""),
-                  ))
+                    myChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "AGE",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          age.toString(),
+                          style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  if (age > 0) {
+                                    age--;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: Icon(
+                                  Icons.remove,
+                                  color: Colors.teal,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 13,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.teal,
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
                 ],
               ),
-            )
+            ),
+            CustomBottomContainer()
           ],
         ),
       ),
@@ -146,10 +320,45 @@ class CustomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: 5,right: 5,bottom: 2,top: 10),
+      margin: EdgeInsets.only(left: 5, right: 5, bottom: 2, top: 10),
       decoration: BoxDecoration(
           color: containerColor, borderRadius: BorderRadius.circular(15)),
       child: myChild,
+    );
+  }
+}
+
+
+class CustomBottomContainer extends StatefulWidget {
+  const CustomBottomContainer({super.key});
+
+  @override
+  State<CustomBottomContainer> createState() => _CustomBottomContainerState();
+}
+
+class _CustomBottomContainerState extends State<CustomBottomContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => ResulClass()));
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 10),
+        height: 60,
+        width: double.infinity,
+        color: Colors.teal,
+        child: Center(
+          child: Text(
+            "CALCULATE BMI",
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/result.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ResulClass extends StatefulWidget {
+  const ResulClass({super.key});
+
+  @override
+  State<ResulClass> createState() => _ResulClassState();
+}
+
+class _ResulClassState extends State<ResulClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        title: Text(
+          "BMI Result",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
